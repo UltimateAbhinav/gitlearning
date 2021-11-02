7c8da8b0e3df6a9f975597cfe1dc1aee65f98339
Abhinav Kumar <masterabhinav2000@gmail.com>
2021-11-02 13:34:46 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,11 @@
+package gitlearn;
+
+public class multiply{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 4;
+        System.out.println("the sum is " + a*b);
+
+        
+    }
+}
